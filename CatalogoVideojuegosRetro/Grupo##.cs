609edba020738eb58f4eade58791ad772bb697d1
Joselyn Ivana Tananta Arevalo <N00433768@upn.pe>
2026-04-16 13:08:30 -0500

    Agregué mis métodos de Estadistica y contarvideojuegos

--- CatalogoVideojuegosRetro/Grupo##.cs
+++ CatalogoVideojuegosRetro/Grupo##.cs
@@ -9,5 +9,59 @@ namespace CatalogoVideojuegosRetro
     internal class Grupo__
     {
         //creacion de las clases asignadas
+        public static int ContarVideojuegos(ListaVideojuegos L)
+        {
+            int contador = 0;
+            Nodo actual = L.Cabeza;
+            //recorrer nodo por nodo
+            while (actual != null)
+            {
+                contador++;
+                actual = actual.Siguiente;
+            }
+            return contador;
+        }
+
+
+
+
+        
+        public static string EstadisticaPrecio(ListaVideojuegos L)
+        {
+            if (L.EstaVacia())
+            {
+                return "La lista está vacía. No se pueden calcular estadísticas.";
+            }
+
+            Nodo actual = L.Cabeza;
+            double min = actual.Dato.Precio;
+            double max = actual.Dato.Precio;
+            double suma = 0;
+            int count = 0;
+
+            while (actual != null)
+            {
+                double precio = actual.Dato.Precio;
+
+                if (precio < min) min = precio;
+                if (precio > max) max = precio;
+
+                suma += precio;
+                count++;
+
+                actual = actual.Siguiente;
+            }
+
+            double promedio = suma / count;
+
+            string resultado =
+                "─── Estadística de Precios ───\r\n" +
+                $"Precio mínimo : S/ {min:F2}\r\n" +
+                $"Precio máximo : S/ {max:F2}\r\n" +
+                $"Precio promedio: S/ {promedio:F2}\r\n" +
+                $"Total videojuegos analizados: {count}";
+
+            return resultado;
+        }
     }
 }

0028f8b56f407d97d278eeef5876eb733250ae70
leonardos12 <75736230+leonardos12@users.noreply.github.com>
2026-04-18 12:36:24 -0500

    Update Grupo##.cs

--- CatalogoVideojuegosRetro/Grupo##.cs
+++ CatalogoVideojuegosRetro/Grupo##.cs
@@ -23,8 +23,50 @@ namespace CatalogoVideojuegosRetro
         }
 
         //2.SonIguales(L1, L2)
+         public static bool SonIguales(ListaVideojuegos L1, ListaVideojuegos L2)
+ {
+     Nodo n1 = L1.Cabeza;
+     Nodo n2 = L2.Cabeza;
+
+     while (n1 != null && n2 != null)
+     {
+         if (n1.Dato.Titulo != n2.Dato.Titulo ||
+             n1.Dato.Genero != n2.Dato.Genero ||
+             n1.Dato.Anio != n2.Dato.Anio ||
+             n1.Dato.Stock != n2.Dato.Stock ||
+             n1.Dato.Precio != n2.Dato.Precio)
+         {
+             return false;
+         }
+
+         n1 = n1.Siguiente;
+         n2 = n2.Siguiente;
+     }
+
+     // Si ambas terminaron al mismo tiempo → son iguales    
+     return n1 == null && n2 == null;
+ }
 
         //3.ConcatenarListas(L1, L2)
+         public static ListaVideojuegos ConcatenarListas(ListaVideojuegos L1, ListaVideojuegos L2)
+ {
+     // Si L1 está vacía, retorna L2
+     if (L1.Cabeza == null)
+         return L2;
+
+     Nodo actual = L1.Cabeza;
+
+     // Ir al último nodo de L1
+     while (actual.Siguiente != null)
+     {
+         actual = actual.Siguiente;
+     }
+
+     // Enlazar con la cabeza de L2
+     actual.Siguiente = L2.Cabeza;
+
+     return L1;
+ }
 
         //4.InvertirLista(L) 
 
